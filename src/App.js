d34319fe99cfc9ee6e0fd1441c5a2d0ea77b783a
Shefy Abraham <shefyabraham@Shefys-MacBook-Air.local>
2021-07-12 16:28:52 +0530

    Removed comments

--- src/App.js
+++ src/App.js
@@ -6,74 +6,32 @@ import Contact from "./components/Contact/index"
 import Footer from "./components/Footer/index"
 import AOS from "aos";
 import "aos/dist/aos.css"
-import { Switch, Route , Router} from "react-router-dom";
+import { Switch, Route, Router } from "react-router-dom";
 import Home from "./components/Home";
 import ScrollToTop from "./components/scrollToTop";
 import AboutInfo from "./components/About/aboutInfo";
 import history from "./components/history";
-/**
- * @author
- * @function App
- **/
 
 const App = (props) => {
 
-  useEffect(()=>{
+  useEffect(() => {
     AOS.init({
-      duration:1000,
+      duration: 1000,
     })
-  },[]);
+  }, []);
 
   return (
-    // <div>
-    //   <Header />
-    //   <Hero />
-    //   <main id="main">
-      /* <Switch>
-              <Route path="/about" exact>
-                <About />
-              </Route>
-              <Route path="/skills" exact>
-                <Skills />
-              </Route>
-              <Route path="/resume" exact>
-                <Resume />
-              </Route>
-              <Route path="/portfolio" exact>
-                <Portfolio />
-              </Route>
-              <Route path="/contact" exact>
-                <Contact />
-              </Route>
-        </Switch> */
-        /* <About />
-        <Facts />
-        <Skills />
-        <Resume />
-        <Portfolio />
-        <Services />
-        <Testimonials />
-         <Contact /> 
-      </main>
-      <Footer />
-    </div> */
     <>
       <Router history={history}>
         <Header />
         <ScrollToTop />
         <Switch>
           <Route path="/about">
-            <AboutInfo/>
+            <AboutInfo />
           </Route>
-          {/* <Route path="/skills">
-            <Skills />
-          </Route>
-          <Route path="/certifications">
-            <Services />
-          </Route> */}
           <Route path="/resume">
             <Resume />
-          </Route>         
+          </Route>
           <Route path="/portfolio">
             <Projects />
           </Route>
